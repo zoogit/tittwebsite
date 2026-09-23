774363385541354aecad53c2a7816e3177ceb06a
zoogit <zoogit@github.com>
2026-09-23 16:31:37 -0700

    Fix design system Netlify route

--- vite.netlify.config.ts
+++ vite.netlify.config.ts
@@ -4,6 +4,12 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     outDir: "netlify-dist",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        designSystem: "design-system/index.html",
+      },
+    },
   },
   plugins: [react()],
 });
